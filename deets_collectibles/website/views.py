ba77f241fdcbef3f8242453986234987670d4270
Michael Cassell <154381173+Detayel@users.noreply.github.com>
2024-07-13 16:48:07 -0700

    Created models, urls, admin pages and db.

---
+++ deets_collectibles/website/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+def welcome(request):
+    return HttpResponse("Welcome to the new Website!")
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+def store(request):
+    return HttpResponse("Welcome to the store!")
